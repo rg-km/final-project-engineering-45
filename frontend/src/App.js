e2904409b66aa2dd1cab73bd89062b485ad84f41
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-22 21:42:52 +0800

    add fakultas and prodi page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './content/home/home';
 import About from './content/about/about';
 import Fakultas from './content/fakultas/Fakultas';
+import Prodi from './content/prodi/Prodi';
 
 const App = () => {
-	return(
+	return (
 		<Routes>
-			<Route path='/' element={<Home />}/>
+			<Route path='/' element={<Home />} />
 			<Route path='RightWay' element={<About />} />
 			<Route path='fakultas'>
 				<Route index element={<Fakultas />} />
+				<Route path=':name' element={<Prodi />} />
 			</Route>
 		</Routes>
-	)  
+	)
 }
 
 export default App;
