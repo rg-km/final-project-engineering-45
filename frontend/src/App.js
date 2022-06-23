2cd0eeeb687b1f79de7da6bc13f6a8bfff82d8fe
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-23 22:36:24 +0800

    fix missing file from panduan page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./content/home/home";
 import About from "./content/about/about";
-import Panduan from './content/panduan/Panduan';
+import Fakultas from "./content/fakultas/Fakultas";
+import Prodi from "./content/prodi/Prodi";
+import ProdiDetail from "./content/prodiDetail/ProdiDetail";
+import Panduan from "./content/panduan/Panduan";
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="RightWay" element={<About />} />
+			<Route path="fakultas">
+				<Route index element={<Fakultas />} />
+				<Route path=":name">
+					<Route index element={<Prodi />} />
+					<Route path=":prodi" element={<ProdiDetail />} />
+				</Route>
+			</Route>
 			<Route path="panduan">
 				<Route index element={<Panduan />} />
 			</Route>
