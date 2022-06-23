3ee53605c88106f5be2187599eed053af9c36be7
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-23 21:04:30 +0800

    add panduan page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom';
-import Home from './content/home/home';
-import About from './content/about/about';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./content/home/home";
+import About from "./content/about/about";
+import Panduan from './content/panduan/Panduan';
 
 const App = () => {
-	return(
+	return (
 		<Routes>
-			<Route path='/' element={<Home />}/>
-			<Route path='RightWay' element={<About />} />
+			<Route path="/" element={<Home />} />
+			<Route path="RightWay" element={<About />} />
+			<Route path="panduan">
+				<Route index element={<Panduan />} />
+			</Route>
 		</Routes>
-	) 
-}
+	);
+};
 
 export default App;
---
+++ frontend/src/content/panduan/MenuItem.js
@@ -0,0 +1,22 @@
+export const MenuItem = [
+	{
+		title: "Panduan Masuk PTN",
+		desc: "Panduan ini berisi persiapan untuk masuk ke Perguruan Tinggi Negeri, seperti test apa saja yang di berikan untuk masuk ke PTN",
+		to: "masuk_ptn"
+	},
+	{
+		title: "Panduan Masuk PTS",
+		desc: "Berisi panduan untuk masuk ke PTS yang ada, dan bagaimana sih cara masuk kedalam PTS yang kita inginkan",
+		to: "masuk_pts"
+	},
+	{
+		title: "Panduan Masuk Kedinasan",
+		desc: "Berisi panduan lengkap untuk siapa saja yang ingin masuk kedalam kedinasan, dengan berbagai macam cara masuk yang tersedia",
+		to: "masuk_kedinasan"
+	},
+	{
+		title: "Panduan Masuk Pendidikan Vokasi",
+		desc: "Panduan lengkap untuk kalian yang ingin masuk kedalam pendidikan vokasi, akan kami berikan panduan yang di perlukan oleh anda",
+		to: "masuk_vokasi"
+	}
+]
