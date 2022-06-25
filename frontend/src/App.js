ac9a9e1dedb1fb62ea6182112ae84d3bbd8cf7f5
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-25 23:44:58 +0800

    add login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,15 @@ import Fakultas from "./content/fakultas/Fakultas";
 import Prodi from "./content/prodi/Prodi";
 import ProdiDetail from "./content/prodiDetail/ProdiDetail";
 import Panduan from "./content/panduan/Panduan";
+import Login from './content/login-and-register/Login';
+import Register from './content/login-and-register/Register';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+		    <Route path='/login' element={<Login />} />
+		    <Route path='/register' element={<Register />} />
 			<Route path="RightWay" element={<About />} />
 			<Route path="fakultas">
 				<Route index element={<Fakultas />} />
