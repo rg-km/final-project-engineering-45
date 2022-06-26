1de25f6a0aa963d01388b657628f2da7a056b35e
Nyoman Trimakna <99968326+Nyomantrimakna@users.noreply.github.com>
2022-06-26 21:01:34 +0800

    add icon to add prodi in prodi.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,9 @@ import Fakultas from "./content/fakultas/Fakultas";
 import Prodi from "./content/prodi/Prodi";
 import ProdiDetail from "./content/prodiDetail/ProdiDetail";
 import Panduan from "./content/panduan/Panduan";
-<<<<<<< HEAD
 import Create from './content/crud/create/Create';
-=======
 import Login from './content/login-and-register/Login';
 import Register from './content/login-and-register/Register';
->>>>>>> main
 
 const App = () => {
 	return (
