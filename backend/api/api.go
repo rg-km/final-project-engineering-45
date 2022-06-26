a0779dc7c174c08e3e25209b94f61d865c9f32de
althafariq <althafariq04@gmail.com>
2022-06-26 19:32:18 +0700

    fix api list

--- backend/api/api.go
+++ backend/api/api.go
@@ -23,18 +23,33 @@ func NewAPI(usersRepo repository.UserRepository, fakultasRepo repository.Fakulta
 		mux,
 	}
 
+	// backend sudah menerapkan autentikasi, tetapi frontend belum dapat mengimplementasikan, sehingga endpoint ini di disable autentikasinya
+	// mux.Handle("/api/user/login", api.POST(http.HandlerFunc(api.login)))
+	// mux.Handle("/api/user/logout", api.POST(http.HandlerFunc(api.logout)))
+	// mux.Handle("/api/user/signup", api.POST(http.HandlerFunc(api.signup)))
+
+	// mux.Handle("/api/fakultas/list", api.GET(api.AuthMiddleWare((http.HandlerFunc(api.fakultasList)))))
+	// //mux.Handle("/api/fakultas", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.fakultasListByName))))
+	// mux.Handle("/api/prodi/list", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.prodiList))))
+	// mux.Handle("/api/prodi", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.prodiListByFakultasName))))
+	// mux.Handle("/api/prodiName", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.selectProdiByName))))
+	// mux.Handle("/api/select_prodi", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.selectProdi))))
+
+	// mux.Handle("/api/prodi/add", api.POST(api.AdminMiddleware(http.HandlerFunc(api.addProdi))))
+
 	mux.Handle("/api/user/login", api.POST(http.HandlerFunc(api.login)))
 	mux.Handle("/api/user/logout", api.POST(http.HandlerFunc(api.logout)))
 	mux.Handle("/api/user/signup", api.POST(http.HandlerFunc(api.signup)))
 
-	mux.Handle("/api/fakultas/list", api.GET(api.AuthMiddleWare((http.HandlerFunc(api.fakultasList)))))
+	mux.Handle("/api/fakultas/list", api.GET(http.HandlerFunc(api.fakultasList)))
 	//mux.Handle("/api/fakultas", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.fakultasListByName))))
-	mux.Handle("/api/prodi/list", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.prodiList))))
-	mux.Handle("/api/prodi", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.prodiListByFakultasName))))
-	mux.Handle("/api/prodiName", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.selectProdiByName))))
-	mux.Handle("/api/select_prodi", api.GET(api.AuthMiddleWare(http.HandlerFunc(api.selectProdi))))
+	mux.Handle("/api/prodi/list", api.GET(http.HandlerFunc(api.prodiList)))
+	mux.Handle("/api/prodi", api.GET(http.HandlerFunc(api.prodiListByFakultasName)))
+	mux.Handle("/api/prodiName", api.GET(http.HandlerFunc(api.selectProdiByName)))
+	mux.Handle("/api/select_prodi", api.GET(http.HandlerFunc(api.selectProdi)))
 
-	mux.Handle("/api/prodi/add", api.POST(api.AdminMiddleware(http.HandlerFunc(api.addProdi))))
+	mux.Handle("/api/prodi/add", api.POST(http.HandlerFunc(api.addProdi)))
+
 	return api
 }
 
--- backend/api/middleware.go
+++ backend/api/middleware.go
@@ -71,13 +71,10 @@ func (api *API) AuthMiddleWare(next http.Handler) http.Handler {
 			return
 		}
 
-		// match token request to token database
-		if c.Value == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImFkbWluIiwiUm9sZSI6ImFkbWluIiwiZXhwIjoxNjU1OTY5OTU1fQ.ekcmkFu7jUWkQObe-Rs3TZFe8NrIyU4-4dTkegP56g8" || tkn.Valid {
-			ctx := context.WithValue(r.Context(), "username", claims.Username)
-			ctx = context.WithValue(ctx, "role", claims.Role)
-			ctx = context.WithValue(ctx, "props", claims)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		}
+		ctx := context.WithValue(r.Context(), "username", claims.Username)
+		ctx = context.WithValue(ctx, "role", claims.Role)
+		ctx = context.WithValue(ctx, "props", claims)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
